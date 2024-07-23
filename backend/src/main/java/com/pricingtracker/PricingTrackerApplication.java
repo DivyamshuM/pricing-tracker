9cf5cc540337c8b9c637640de54953346dc1a38a
DivyamshuM <divyamshu287@gmail.com>
2024-07-22 19:02:00 -0700

    Initial commit with backend setup

---
+++ backend/src/main/java/com/pricingtracker/PricingTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.pricingtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricingTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricingTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pricingtracker/PricingTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pricingtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
